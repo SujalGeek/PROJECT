b73c89ca104f9eda730e8ebd881ba396b86f5c02
Sujal <morwanisujal875@gmail.com>
2024-01-18 22:48:39 +0530

    Datatype Conversion in Javascript

---
+++ day-05/second.js
@@ -0,0 +1,86 @@
+let age=28;
+let LoggedIn =false;
+let start;
+BigInt =2;
+String ="23232";
+console.log(typeof null);
+console.log(typeof undefined);
+/*
+Undefined apne aap ma ek type hai
+where as ek object hai
+*/
+let score2 ="33";
+let s="3331ab";
+let valuenum =Number(score2);
+console.log( typeof valuenum);
+let valuenum1=Number(s);
+console.log(typeof valuenum1);
+console.log(valuenum1);
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
