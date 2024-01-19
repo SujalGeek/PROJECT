750b49c0a50a231c97a62dc4cd58fd20ca68024a
Sujal <morwanisujal875@gmail.com>
2024-01-19 23:12:21 +0530

    Conditional operators and datatypes in javascript

---
+++ day-06/third.js
@@ -0,0 +1,40 @@
+let val=3;
+let negval=-val;
+console.log(negval);
+
+
+console.log(2+3);
+console.log(2*2);
+console.log(2-2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%3);
+
+let ar1="hello";
+let ar2=" sujal";
+console.log(ar1+ar2);
+console.log("It's another way");
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
