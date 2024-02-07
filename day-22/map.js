02d75a17e9ba1f0427789da774c39c4dbdd5c226
Sujal <morwanisujal875@gmail.com>
2024-02-07 19:56:53 +0530

    Dom in Js

--- day-22/map.js
+++ day-22/map.js
@@ -1,28 +1,28 @@
-// const coding = ["js", "ruby", "java", "python", "cpp"]
+const coding = ["js", "ruby", "java", "python", "cpp"]
 
 
-// const values = coding.forEach( (item) => {
-//     //console.log(item);
-//     return item
-// } )
+const values = coding.forEach( (item) => {
+    //console.log(item);
+    return item
+} )
 
-// console.log(values);
+console.log(values);
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num) => {
-//     return num > 4
-// } )
+const newNums = myNums.filter( (num) => {
+    return num > 4
+} )
 
-// const newNums = []
+const newNums2 = []
 
-// myNums.forEach( (num) => {
-//     if (num > 4) {
-//         newNums.push(num)
-//     }
-// } )
+myNums.forEach( (num) => {
+    if (num > 4) {
+        newNums.push(num)
+    }
+} )
 
-// console.log(newNums);
+console.log(newNums);
 
 
 const books = [
