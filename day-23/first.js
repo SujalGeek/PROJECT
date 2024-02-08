ab94244e6224eda15568de9a7fea26afafea6343
Sujal <morwanisujal875@gmail.com>
2024-02-08 19:18:41 +0530

    DOM in JS

---
+++ day-23/first.js
@@ -0,0 +1,7 @@
+let firstName ="Sujal"; // String data type bhai saheb
+let student = true ; // Boolean data type bhai
+//console.log("Hello" ,age);
+console.log("My name is", firstName);
+console.log("I have been elected" ,student);
+document.getElementById('heading').innerHTML="Hi " + firstName;
+document.getElementById('heading').getAttribute('id')
